4bbb0027427586af2c3e30ca182d785fbd9baf3f
crehds <carlos15erh@gmail.com>
2021-01-16 01:59:52 -0500

    Estilando NavBar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { DivLogo, DivTitleLogo, H3, Nav, P } from './styles';
-import logo from '../../assets/hagaloverde_logo.png';
+import logo from '../../assets/hagaloverde_logo_86x90.png';
 import { Menu } from '../Menu/index';
 
 export const NavBar = () => {
--- src/components/Navbar/styles.jsx
+++ src/components/Navbar/styles.jsx
@@ -5,8 +5,8 @@ export const Nav = styled.nav`
   height: 100%;
   display: flex;
   justify-content: space-between;
-  padding: 15px 10px;
-  padding-left: 40px; 
+  padding: 15px 20px;
+  padding-left: 60px; 
 `
 
 export const DivLogo = styled(LinkRouter)`
@@ -20,15 +20,17 @@ export const DivLogo = styled(LinkRouter)`
 
 export const DivTitleLogo = styled.div`
   max-width:100%;
+  margin: auto 0;
 `
 
 export const H3 = styled.h3`
   padding-top: 5px;
-  font-family: 'Lato';
+  font-family: 'Noto Sans JP';
+  font-weight: 400;
 `
 
 export const P = styled.p`
-  font-family: 'Lato';
+  font-family: 'Noto Sans JP';
   font-weight: 300;
   font-size: 15px;
 `
